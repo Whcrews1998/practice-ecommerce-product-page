92ffd716240cccb2a23acab95a9ad33168630705
Harris Crews <whcrews1998@gmail.com>
2026-02-23 15:32:04 -0500

    Implemented basic html structure.

--- frontend/src/components/ProductPage/index.jsx
+++ frontend/src/components/ProductPage/index.jsx
@@ -3,11 +3,53 @@ import { useState } from "react";
 import style from "./ProductPage.module.css";
 
 export default function ProductPage() {
+  const [purchaseAmount, setPurchaseAmount] = useState(0);
+
   return (
     <div className={style["container"]}>
       <Navbar />
-      <div className={style["product-images"]}></div>
-      <div className={style["product-details"]}></div>
+      <div className={style["product-images"]}>{/* IMPLEMENT */}</div>
+      <div className={style["product-details"]}>
+        <h3>Elegant Luxury Watch</h3>
+        <p>Prestige Time Co.</p>
+        <div className={style["prices"]}>
+          <span className={style["current-price"]}>$499.99</span>
+          <span className={style["original-price"]}>$799.99</span>
+          <span className={style["discount"]}>38% off</span>
+        </div>
+        <p className={style["product-description"]}>
+          This luxurious timepiece combines elegance with precision
+          craftsmanship. Perfect for formal occasions or everyday wear, it
+          features premium materials and impeccable design.
+        </p>
+        <div className={style["ratings"]}>{/* IMPLEMENT */}</div>
+        <div className={style["purchase-amount"]}>
+          <button
+            type="button"
+            onClick={() =>
+              setPurchaseAmount(purchaseAmount > 0 ? purchaseAmount - 1 : 0)
+            }
+          >
+            -
+          </button>
+          <input
+            type="number"
+            name="amount"
+            value={purchaseAmount}
+            onChange={(e) => setPurchaseAmount(e.target.value)}
+          />
+          <button
+            type="button"
+            onClick={() => setPurchaseAmount(purchaseAmount + 1)}
+          >
+            +
+          </button>
+        </div>
+        <button className={style["add-to-bag-btn"]} type="button">
+          Add to Bag
+        </button>
+        <div className={style["offer-container"]}>{/* IMPLEMENT */}</div>
+      </div>
     </div>
   );
 }
