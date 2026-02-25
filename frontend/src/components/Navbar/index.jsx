db9f8ff3951855a1115fba0a9667961de51f5f42
Harris Crews <whcrews1998@gmail.com>
2026-02-25 16:26:51 -0500

    Updated Navbar width size.

--- frontend/src/components/Navbar/index.jsx
+++ frontend/src/components/Navbar/index.jsx
@@ -4,36 +4,38 @@ import style from "./Navbar.module.css";
 export default function Navbar() {
   const [searchBar, setSearchBar] = useState("");
   return (
-    <header className={style["container"]}>
-      <div className={style["logo"]}>Timeless Watches</div>
-      <div className={style["search-bar"]}>
-        <input
-          type="text"
-          name="search-bar"
-          value={searchBar}
-          placeholder="Search..."
-          onChange={(e) => setSearchBar(e.target.value)}
-        />
+    <header className={style["navbar"]}>
+      <div class={style["navbar-container"]}>
+        <div className={style["logo"]}>Timeless Watches</div>
+        <div className={style["search-bar"]}>
+          <input
+            type="text"
+            name="search-bar"
+            value={searchBar}
+            placeholder="Search..."
+            onChange={(e) => setSearchBar(e.target.value)}
+          />
+        </div>
+        <nav>
+          <ul>
+            <li>
+              <a href="#">Shop</a>
+            </li>
+            <li>
+              <a href="#">New Arrivals</a>
+            </li>
+            <li>
+              <a href="#">Brands</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+          </ul>
+        </nav>
       </div>
-      <nav>
-        <ul>
-          <li>
-            <a href="#">Shop</a>
-          </li>
-          <li>
-            <a href="#">New Arrivals</a>
-          </li>
-          <li>
-            <a href="#">Brands</a>
-          </li>
-          <li>
-            <a href="#">About</a>
-          </li>
-          <li>
-            <a href="#">Contact</a>
-          </li>
-        </ul>
-      </nav>
     </header>
   );
 }
